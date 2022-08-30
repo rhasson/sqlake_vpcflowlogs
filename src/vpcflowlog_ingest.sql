911da56ebfe555c7a4b381f3db8f6c652f0697ef
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-29 22:51:52 -0400

    sql test

--- src/vpcflowlog_ingest.sql
+++ src/vpcflowlog_ingest.sql
@@ -1,24 +1,24 @@
 
 /* Create read-only connection to S3 data source */
-create s3 connection royon_s3_connection
+create s3 connection royon_s3_conn
     aws_role = 'arn:aws:iam::433987883887:role/royon_ro_role'
     external_id = 'ROYON'
     read_only = TRUE
 ;
 
 /* create staging table in the Glue data catalog */
-create table default_glue_catalog.royon.vpcflowlogs_stage_tbl;
+create table default_glue_catalog.royon.vpcflowlogs_staging_tbl;
 
 /* create ingestion job to stage the S3 data in Glue table */
-create job royon_stage_vpc_flowlogs
+create job royon_staging_vpc_flowlogs
   content_type = auto
-  as copy from s3 royon_s3_connection
+  as copy from s3 royon_s3_conn
     bucket = 'dbxflowlog'
     prefix = 'AWSLogs/aws-account-id=433987883887/aws-service=vpcflowlogs/aws-region=us-east-1'
-  into default_glue_catalog.royon.vpcflowlogs_stage_tbl;
+  into default_glue_catalog.royon.vpcflowlogs_staging_tbl;
 ;
 
 -- clean up previous version of the resources
 drop job royon_stage_vpc_flowlogs;
 drop table default_glue_catalog.royon.vpcflowlogs_stage_tbl;
-drop s3 connection royon_s3_conn;
+drop connection royon_s3_conn;
