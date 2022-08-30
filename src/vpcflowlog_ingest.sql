4ac085d01b89bc4e94096955cafc90594ea11e0a
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-30 12:26:53 -0400

    fix event_date

--- src/vpcflowlog_ingest.sql
+++ src/vpcflowlog_ingest.sql
@@ -8,7 +8,7 @@ create s3 connection royon_s3_conn
 
 /* create staging table in the Glue data catalog */
 create table default_glue_catalog.royon.vpcflowlogs_staging_tbl
-partitioned by $event_time;
+partitioned by $event_date;
 
 /* create ingestion job to stage the S3 data in Glue table */
 create job royon_staging_vpc_flowlogs
