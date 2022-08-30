1420850fde313549b09a053e87fc60a3a22b9639
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-30 12:41:18 -0400

    fix drop table command

--- src/vpcflowlog_ingest.sql
+++ src/vpcflowlog_ingest.sql
@@ -8,7 +8,7 @@ create s3 connection royon_s3_conn
 ;
 
 /* create staging table in the Glue data catalog */
-drop table default_glue_catalog.royon.vpcflowlogs_staging_tbl;
+drop table default_glue_catalog.royon.vpcflowlogs_staging_tbl DELETE_DATA=TRUE;
 create table default_glue_catalog.royon.vpcflowlogs_staging_tbl
 partitioned by $event_date;
 
