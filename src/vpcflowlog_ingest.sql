466a446f6fc30b998468d1d70446ca2a9650b9c7
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-08-30 14:59:10 -0400

    clean up sql

--- src/vpcflowlog_ingest.sql
+++ src/vpcflowlog_ingest.sql
@@ -1,8 +1,3 @@
--- pre-clean
-drop table default_glue_catalog.royon.vpcflowlogs_staging_tbl DELETE_DATA=TRUE;
-drop connection royon_s3_conn;
-
-
 /* Create read-only connection to S3 data source */
 create s3 connection royon_s3_conn
     aws_role = 'arn:aws:iam::433987883887:role/royon_ro_role'
