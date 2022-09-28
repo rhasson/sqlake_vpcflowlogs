f23607b771d87158239b37de147cfa2592713230
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-28 10:09:02 -0400

    changed API endpoint and updated SQL to test

--- src/001-connections/royon_s3_conn.sql
+++ src/001-connections/royon_s3_conn.sql
@@ -1,5 +1,5 @@
 /* Create read-only connection to S3 data source */
-create s3 connection royon_s3_conn
+create s3 connection royon_s3_conn2
     aws_role = 'arn:aws:iam::433987883887:role/royon_ro_role'
     external_id = 'ROYON'
     read_only = TRUE
