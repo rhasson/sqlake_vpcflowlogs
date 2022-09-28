c01eace7eb63a68e749bafd8903d9f2229a9aef0
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-28 13:18:00 -0400

    create ingest job for s3 access logs

---
+++ src/003-jobs/royon_staging_s3_access.sql
@@ -0,0 +1,7 @@
+/* create ingestion job to stage S3 access logs in data lake */
+create job royon_staging_s3_access
+  as copy from s3 logs_bucket_conn
+    bucket = 'dbxflowlogs3logging'
+    prefix = '/'
+  into default_glue_catalog.royon.s3access_staging_tbl
+;
