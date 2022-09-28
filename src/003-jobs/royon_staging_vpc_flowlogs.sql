193742420193141a698e9e235ab8c07e05ff91a0
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-28 13:28:52 -0400

    create ingest job for vpcflow logs

--- src/003-jobs/royon_staging_vpc_flowlogs.sql
+++ src/003-jobs/royon_staging_vpc_flowlogs.sql
@@ -1,10 +1,11 @@
-/* create ingestion job to stage the S3 data in Glue table */
+/* create ingestion job to stage VPC flow logs */
 create job royon_staging_vpc_flowlogs
-  start_from = NOW
-  date_pattern = 'yyyy/MM/dd/HH/mm'
-  content_type = parquet
-  as copy from s3 royon_s3_conn
+  COMPUTE_CLUSTER = "Default Compute"
+  --start_from = NOW
+  --date_pattern = 'yyyy/MM/dd/HH/mm'
+  --content_type = parquet
+  as copy from s3 logs_bucket_conn
     bucket = 'dbxflowlog'
     prefix = 'AWSLogs/433987883887/vpcflowlogs/us-east-1'
-  into default_glue_catalog.royon.vpcflowlogs_staging_tbl;
+  into default_glue_catalog.royon.vpcflowlogs_staging_tbl
 ;
