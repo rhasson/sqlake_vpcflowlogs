532adedea4cfac309d25794224802601e3017064
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-28 14:35:32 -0400

    create ingest job for s3 access logs

--- src/003-jobs/royon_staging_s3_access.sql
+++ src/003-jobs/royon_staging_s3_access.sql
@@ -1,8 +1,15 @@
 /* create ingestion job to stage S3 access logs in data lake */
 create job royon_staging_s3_access
   COMPUTE_CLUSTER = "Default Compute"
+  START_FROM = BEGINNING
+  CONTENT_TYPE = (
+    TYPE = REGEX
+    PATTERN = '^(?<bucketowner>.*?)\s(?<bucket>.*?)\s(?<requesttime>\[.*?\])\s(?<remoteip>.*?)\s(?<requester>.*?)\s(?<requestid>.*?)\s(?<operation>.*?)\s(?<requestkey>.*?)\s(?<requesturi>\".*?\")\s(?<httpstatus>.*?)\s(?<errorcode>.*?)\s(?<bytessent>.*?)\s(?<objectsize>.*?)\s(?<totaltime>.*?)\s(?<turnaroundtime>.*?)\s(?<referer>\".*?\")\s(?<useragent>\".*?\")\s(?<versionid>.*?)$'
+    MULTI_LINE = TRUE
+    INFER_TYPES = FALSE
+  )
   as copy from s3 logs_bucket_conn
     bucket = 'dbxflowlogs3logging'
-    prefix = '/'
+    prefix = ''
   into default_glue_catalog.royon.s3access_staging_tbl
 ;
