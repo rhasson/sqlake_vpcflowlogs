a1568e058c5059965b51891f38f8e4999498e433
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-28 18:34:22 -0400

    create refined table for modeled s3access logs

---
+++ src/002-tables/s3access_refined_tbl.sql
@@ -0,0 +1,24 @@
+/* create a refined table for S3 access logs after it is modeled */
+create table default_glue_catalog.royon.s3access_refined_tbl (
+    bucket_owner_id string,
+    bucket_name string,
+    request_time_utc date,
+    remote_ip string,
+    requester_id string,
+    request_id string,
+    operation string,
+    request_key string,
+    request_uri string,
+    http_status string,
+    error_code string,
+    bytes_sent bigint,
+    object_size bigint,
+    total_time_ms bigint,
+    turn_around_time_ms bigint,
+    referer string,
+    user_agent string,
+    version_id string
+  )
+  partitioned by request_time_utc
+COMPUTE_CLUSTER = 'Default Compute'
+;
