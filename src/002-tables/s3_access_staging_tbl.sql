1a5fc403f60aeef5477ed22474d2bfa00a374639
Roy Hasson <royhasson@roys-mbp-2.lan>
2022-09-28 12:58:40 -0400

    add target tables for s3 access and vpcflow logs

--- src/002-tables/s3_access_staging_tbl.sql
+++ src/002-tables/s3_access_staging_tbl.sql
@@ -1,4 +1,5 @@
 /* create staging table for S3 access logs */
 create table default_glue_catalog.royon.s3access_staging_tbl
   partitioned by $event_date
+COMPUTE_CLUSTER = "Default Compute"
 ;
--- src/002-tables/vpcflowlogs_staging_tbl.sql
+++ src/002-tables/vpcflowlogs_staging_tbl.sql
@@ -1,4 +1,5 @@
 /* create staging table for vpc flow logs */
 create table default_glue_catalog.royon.vpcflowlogs_staging_tbl
   partitioned by $event_date
+COMPUTE_CLUSTER = "Default Compute"
 ;
